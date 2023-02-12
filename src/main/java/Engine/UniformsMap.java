b55c55450b55f374932b0ffb9a3a9f87e3037f09
AllenXeon07 <franciscoallenxeon@gmail.com>
2023-02-12 11:23:34 +0700

    rev uniformMap and window

--- src/main/java/Engine/UniformsMap.java
+++ src/main/java/Engine/UniformsMap.java
@@ -47,4 +47,8 @@ public class UniformsMap {
     public void setUniform(String uniformName, Vector4f value) {
         glUniform4f(getUniformLocation(uniformName), value.x, value.y, value.z, value.w);
     }
+
+    public void setUniform(String uniformName, Vector3f value) {
+        glUniform3f(getUniformLocation(uniformName), value.x, value.y, value.z);
+    }
 }
--- src/main/java/Engine/Window.java
+++ src/main/java/Engine/Window.java
@@ -1,15 +1,10 @@
 package Engine;
 
 import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.system.MemoryStack;
-
-import java.nio.IntBuffer;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
@@ -58,10 +53,10 @@ public class Window {
         if ( !glfwInit() )
             throw new IllegalStateException("Unable to initialize GLFW");
 
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+//        // Configure GLFW
+//        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+//        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+//        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
         // Create the window
         window = glfwCreateWindow(width+2, height+2, title, NULL, NULL);
@@ -69,30 +64,30 @@ public class Window {
             throw new RuntimeException("Failed to create the GLFW window");
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-        });
-
-        // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
+//        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+//            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+//                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+//        });
+//
+//        // Get the thread stack and push a new frame
+//        try ( MemoryStack stack = stackPush() ) {
+//            IntBuffer pWidth = stack.mallocInt(1); // int*
+//            IntBuffer pHeight = stack.mallocInt(1); // int*
+//
+//            // Get the window size passed to glfwCreateWindow
+//            glfwGetWindowSize(window, pWidth, pHeight);
+//
+//            // Get the resolution of the primary monitor
+//            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+//
+//            // Center the window
+//            glfwSetWindowPos(
+//                    window,
+//                    (vidmode.width() - pWidth.get(0)) / 2,
+//                    (vidmode.height() - pHeight.get(0)) / 2
+//            );
+//        } // the stack frame is popped automatically
+//
         // Make the OpenGL context current
         glfwMakeContextCurrent(window);
         // Enable v-sync
